e033c03ec01a177f8e429379a99f7470f2f80193
Pratheesh Eswaran <pratheesh@Pratheeshs-MacBook-Pro.local>
2019-12-26 23:26:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Parallax from 'react-springy-parallax';
 
-function App() {
-  return (
+let data = require('./data.json');
+
+class App extends Component {
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+      {/* <div className="content"> */}
+      <Parallax ref='parallax' pages={5} >
+   <div>
+   <div className='row'>
+    <div className='column'>
+      <div class='one-column'>
+        John
+      </div>
     </div>
-  );
+    <div class='column'>
+      <div class='two-column'>
+        Steinbeck
+      </div>
+    </div>
+    <div class='column'>
+      <div class='three-column'>
+        Words
+      </div>
+    </div>
+  </div>
+   </div>
+        {data.map((quote) => (
+          <Parallax.Layer
+            offset={quote.offset}
+            speed={quote.speed}>
+            <span id={quote.id} style={{color:quote.color}} >
+              {quote.quote}
+            </span>
+
+          </Parallax.Layer>
+        ))}
+
+      </Parallax>
+      {/* </div> */}
+
+    </div>
+    )
+  }
 }
 
 export default App;
